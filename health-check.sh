92d52658439671fe828ec1492b130c48da7ec019
madeatinvu <madelaine@invupos.com>
2025-03-24 12:40:11 -0500

    changes

---
+++ health-check.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+KEYSARRAY=()
+URLSARRAY=()
+
+urlsConfig="$(pwd)/urls.cfg"
+echo "Reading $urlsConfig"
+while read -r line
+do
+  echo "  $line"
+  IFS='=' read -ra TOKENS <<< "$line"
+  KEYSARRAY+=(${TOKENS[0]})
+  URLSARRAY+=(${TOKENS[1]})
+done < "$urlsConfig"
+
+echo "***********************"
+echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
+
+mkdir -p public/logs
+
+for (( index=0; index < ${#KEYSARRAY[@]}; index++))
+do
+  key="${KEYSARRAY[index]}"
+  url="${URLSARRAY[index]}"
+  echo "  $key=$url"
+
+  for i in 1 2 3 4; 
+  do
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
+      result="success"
+    else
+      result="failed"
+    fi
+    if [ "$result" = "success" ]; then
+      break
+    fi
+    sleep 5
+  done
+  dateTime=$(date +'%Y-%m-%d %H:%M')
+  echo $dateTime, $result >> "public/logs/${key}_report.log"
+done
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const cron = require('node-cron');
+
+const app = express();
+const port = 3030;
+
+// Serve static files (HTML, CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run health check script every minute '30 * * * *' cada 30 minutos
+cron.schedule('* * * * *', () => {
+  exec('./health-check.sh', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+});
+
+app.get('/urls', (req, res) => {
+    const urlsPath = path.join(__dirname, 'urls.cfg');
+    if (fs.existsSync(urlsPath)) {
+      const urls = fs.readFileSync(urlsPath, 'utf8');
+      res.send(urls);
+    } else {
+      res.status(404).send('urls.cfg not found');
+    }
+  });
+  
+  
+// Read logs from the log file and serve it to the front-end
+app.get('/status', (req, res) => {
+  const logs = fs.readFileSync('./logs/key_report.log', 'utf8');
+  res.send(logs);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
